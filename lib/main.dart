4dd72a7c6cff6caf2ec931240af56ae422737852
omoma ig <omomaighawosa440@gmail.com>
2024-02-10 12:37:56 +0100

    add title section and video image widget in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodrecipe/widgets/Appbar.dart';
+import 'package:foodrecipe/widgets/Video.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,12 +32,27 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Appbar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "How to make french toast",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 17,
+              ),
+              VideoImage(bgImage: "assets/images/image_13.png"),
+              const SizedBox(
+                height: 14,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
-
-
-
-
-
-
